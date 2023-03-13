40907e765d7977e4cfd71bb5f7630b1f8c559577
WagnerRodrigues <felten19@gmail.com>
2023-03-13 18:50:43 -0300

    Criado codigo sql com o knex para o movieNotes e lançado no banco de dados

--- src/database/knex/migrations/20230313213220_movieNotes.js
+++ src/database/knex/migrations/20230313213220_movieNotes.js
@@ -1,15 +1,13 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("movieNotes", table => {
+  table.increments("id");
+  table.text("title");
+  table.text("description");
+  table.integer("rating");
+  table.integer("user_id").references("id").inTable("users");
+
+  table.timestamp("created_at").default(knex.fn.now());
+  table.timestamp("updated_at").default(knex.fn.now());
+});
+
+exports.down = knex => knex.schema.dropTable("movieNotes");
