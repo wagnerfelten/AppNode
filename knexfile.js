d0fb927ca780ff7d9fc9a5cab3fb0a0fd86c3c5d
WagnerRodrigues <felten19@gmail.com>
2023-03-13 18:33:32 -0300

    Criado migrate para movieNotes com o knex

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
-    useNullAsDefault: true
+    migrations:{
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true,
   }
 };
