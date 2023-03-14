04ac07634bdb227094fcfcef72bd77b1a2a42239
WagnerRodrigues <felten19@gmail.com>
2023-03-14 09:11:19 -0300

    Criada funcionalidade de delete de notas em cascata, quando deletar uma nota vai deletar as notas vinculadas a ela

--- src/database/knex/migrations/20230313215702_movieTags.js
+++ src/database/knex/migrations/20230313215702_movieTags.js
@@ -1,7 +1,7 @@
 exports.up = knex => knex.schema.createTable("movieTags", table => {
     table.increments("id");
     table.text("name");
-    table.integer("note_id").references("id").inTable("movieNotes");
+    table.integer("note_id").references("id").inTable("movieNotes").onDelete("CASCADE");
     table.integer("user_id").references("id").inTable("users");
 });
 
